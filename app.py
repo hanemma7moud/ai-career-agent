f036639d7e9b163c76645c6fdd0f3d14aca13e21
Hanem Ellethy <hanemma7moud@gmail.com>
2026-05-26 09:26:27 +1000

    Refactor response handling with new OpenAI client
    Updated the response handling to use the new OpenAI client and modified the agent reference details.

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ client = AzureOpenAI(
     api_version="2024-02-15-preview",
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 )
+my_agent = "AI-Career-Agent"
+my_version = "6"
 
-response = client.chat.completions.create(
-    model=os.getenv("DEPLOYMENT_NAME"),  # IMPORTANT
-    messages=[
-        {"role": "system", "content": "You are a helpful AI career assistant."},
-        {"role": "user", "content": "Tell me what you can help with"}
-    ]
+openai_client = project_client.get_openai_client()
+
+# Reference the agent to get a response
+response = openai_client.responses.create(
+    input=[{"role": "user", "content": "Tell me what you can help with."}],
+    extra_body={"agent_reference": {"name": my_agent, "version": my_version, "type": "agent_reference"}},
 )
 
-print(response.choices[0].message.content)
+print(f"Response output: {response.output_text}")
+
