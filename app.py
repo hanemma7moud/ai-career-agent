235f473fff395754ef843f19f8a18c3ef6960527
Hanem Ellethy <hanemma7moud@gmail.com>
2026-05-26 09:22:49 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 import os
-from azure.ai.projects import AIProjectClient
-from azure.identity import DefaultAzureCredential
+from openai import AzureOpenAI
 
-endpoint = "https://csc300-cv.services.ai.azure.com/api/projects/CVAssistant"
-
-project_client = AIProjectClient(
-    endpoint=endpoint,
-    subscription_id=os.getenv("AZURE_SUBSCRIPTION_ID"),
-    resource_group_name=os.getenv("AZURE_RESOURCE_GROUP"),
-    project_name=os.getenv("AZURE_PROJECT_NAME"),
-    credential=DefaultAzureCredential()
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version="2024-02-15-preview",
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 )
 
-openai_client = project_client.get_openai_client()
-
-response = openai_client.responses.create(
-    input=[{
-        "role": "user",
-        "content": "Tell me what you can help with."
-    }],
-    extra_body={
-        "agent_reference": {
-            "name": "AI-Career-Agent",
-            "version": "5",
-            "type": "agent_reference"
-        }
-    }
+response = client.chat.completions.create(
+    model=os.getenv("DEPLOYMENT_NAME"),  # IMPORTANT
+    messages=[
+        {"role": "system", "content": "You are a helpful AI career assistant."},
+        {"role": "user", "content": "Tell me what you can help with"}
+    ]
 )
 
-print(response.output_text)
+print(response.choices[0].message.content)
